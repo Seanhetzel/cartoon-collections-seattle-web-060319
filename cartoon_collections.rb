7d2d4b382c03371a552ff39bfa2ca651d67fab8d
Sean Hetzel <seanhetzel1@gmail.com>
2019-05-17 05:55:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,6 +11,7 @@ def summon_captain_planet(planeteer_calls)
   # Your code here
   planeteer_calls.collect do |element|
     "#{element.capitalize}!"
+  end
 end
 
 def long_planeteer_calls# code an argument here
